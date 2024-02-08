9fc06bfbd17217214c3de0b75903f46a03c4cd6d
Jorge Barrientos <elgigototo@hotmail.com>
2024-02-08 09:57:42 -0600

    first structure

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const app = express();
 const path = require("path");
 
 app.get("/", (req,res)=>{
-    res.sendFile(path.join(__dirname + "/public/index.html") )
+    res.sendFile(path.join(__dirname + "/routes/index.html") )
 });
+
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use(express.static('public'));
 
 app.listen(2077,()=>{
     console.log('Puerto 2077 hosteando app');
